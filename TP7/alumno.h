fa147496cccd8422541ec4e6c1f13ad23e4b03f6
Francisco Cejas <francejas98@gmail.com>
2025-05-12 06:15:57 -0300

    k

---
+++ TP7/alumno.h
@@ -0,0 +1,16 @@
+#ifndef ALUMNO_H_INCLUDED
+#define ALUMNO_H_INCLUDED
+
+typedef struct {
+     int legajo;
+     char nombreYapellido [30];
+     int edad;
+     int anio;
+
+} stAlumno;
+
+///PROTOTIPADOS
+
+
+
+#endif // ALUMNO_H_INCLUDED
--- TP7/main.c
+++ TP7/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "alumno.h"
+
+#define AR_ARCHIVO_INT "int.dat"
 
 int main(int argc, char *argv[])
 {
@@ -11,34 +14,8 @@ int main(int argc, char *argv[])
         switch(selected)
         {
         case 1:
-            funcion_x();
-            break;
-        case 2:
-            funcion_x();
-            break;
-        case 3:
-            funcion_x();
-            break;
-        case 4:
-            funcion_x();
-            break;
-        case 5:
-            funcion_x();
-            break;
-        case 6:
-            funcion_x();
-            break;
-        case 7:
-            funcion_x();
-            break;
-        case 8:
-            funcion_x();
-            break;
-        case 9:
-            funcion_x();
-            break;
-        case 10:
-            funcion_x();
+
+            system("PAUSE");
             break;
         case 0:
             printf("\n\nTERMINATE THE PROGRAM\n");
@@ -61,16 +38,9 @@ int menu()
     printf("\n----------");
     printf("\nLIST MENU");
     printf("\n-----------");
-    printf("\n1-Agrega elementos a Pila");
+    printf("\n1-Hacer una función que agregue un elemento al final de un archivo.");
     printf("\n0-QUIT");
     printf("\n\nENTER YOUR CHOICE: ");
     scanf("%d",&input);
     return input;
-}
-funcion_x()
-{
-    system("cls");
-    system("color 1E");
-    printf("\n\naca deberia estar la funcion pedida en cada ejercicio de la practica\n");
-    system("PAUSE");
 }
